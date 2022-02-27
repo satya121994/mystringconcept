1c5f853038044c9570e3350771d5d2c090010d03
satya121994 <ksatyanarayana615@gmail.com>
2022-02-27 20:36:49 +0530

    new project

---
+++ src/test/java/mystringconcept/StringPractice.java
@@ -0,0 +1,16 @@
+package mystringconcept;
+
+public class StringPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String s1 = "satya"
+				;
+		char c = s1.charAt(3);
+		System.out.println("third char is "+c);
+		
+	}
+
+}
