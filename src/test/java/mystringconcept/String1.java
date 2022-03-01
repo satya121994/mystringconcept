dc85aed23164d2432ec63dfc32bbdfebcc208a1c
satya121994 <ksatyanarayana615@gmail.com>
2022-03-01 17:08:57 +0530

    java

---
+++ src/test/java/mystringconcept/String1.java
@@ -0,0 +1,22 @@
+package mystringconcept;
+
+public class String1 {
+
+	public static void main(String[] args) {
+	
+		String text = "satyanarayana";
+		String pattern ="nara";
+		
+		boolean result =text.contains(pattern);
+		if(result)
+			System.out.println("pattern found");
+		else 
+			System.out.println("not found");
+		
+		
+		
+		
+		
+	}
+
+}
